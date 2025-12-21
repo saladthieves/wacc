3b14fbef7f4e48476368caf02855776e2b2fec4d
saladthieves <saladthieves@gmail.com>
2025-12-21 13:47:52 +0300

    Update AsmWriter tests with new syntax

--- test/backend/asm_writer_test.cpp
+++ test/backend/asm_writer_test.cpp
@@ -77,10 +77,21 @@ TEST_F(AsmWriterTest, writeThrowOnEmptyPath) {
 TEST_F(AsmWriterTest, write) {
     // ARRANGE
     auto lines = make_unique<vector<string>>();
-    lines->push_back("    .glob _main");
-    lines->push_back("_main:");
-    lines->push_back("    movl    $10, %eax");
+    lines->push_back("    .globl main");
+    lines->push_back("main:");
+    lines->push_back("    pushq    %rbp");
+    lines->push_back("    movq    %rsp, %rbp");
+    lines->push_back("    subq    $-8, %rsp");
+    lines->push_back("    movl    $25, -4(%rbp)");
+    lines->push_back("    negl    -4(%rbp)");
+    lines->push_back("    movl    -4(%rbp), %r10d");
+    lines->push_back("    movl    %r10d, -8(%rbp)");
+    lines->push_back("    notl    -8(%rbp)");
+    lines->push_back("    movl    -8(%rbp), %eax");
+    lines->push_back("    movq    %rbp, %rsp");
+    lines->push_back("    popq    %rbp");
     lines->push_back("    ret");
+
     auto writer = AsmWriter{std::move(lines), sampleAsm};
     string error{};
 
@@ -95,8 +106,19 @@ TEST_F(AsmWriterTest, write) {
 
     // ASSERT
     ASSERT_TRUE(error.empty());
-    ASSERT_TRUE(content.contains("    .glob _main\n"));
-    ASSERT_TRUE(content.contains("_main:\n"));
-    ASSERT_TRUE(content.contains("    movl    $10, %eax\n"));
+    
+    ASSERT_TRUE(content.contains("    .globl main\n"));
+    ASSERT_TRUE(content.contains("main:\n"));
+    ASSERT_TRUE(content.contains("    pushq    %rbp\n"));
+    ASSERT_TRUE(content.contains("    movq    %rsp, %rbp\n"));
+    ASSERT_TRUE(content.contains("    subq    $-8, %rsp\n"));
+    ASSERT_TRUE(content.contains("    movl    $25, -4(%rbp)\n"));
+    ASSERT_TRUE(content.contains("    negl    -4(%rbp)\n"));
+    ASSERT_TRUE(content.contains("    movl    -4(%rbp), %r10d\n"));
+    ASSERT_TRUE(content.contains("    movl    %r10d, -8(%rbp)\n"));
+    ASSERT_TRUE(content.contains("    notl    -8(%rbp)\n"));
+    ASSERT_TRUE(content.contains("    movl    -8(%rbp), %eax\n"));
+    ASSERT_TRUE(content.contains("    movq    %rbp, %rsp\n"));
+    ASSERT_TRUE(content.contains("    popq    %rbp\n"));
     ASSERT_TRUE(content.contains("    ret\n"));
 }
