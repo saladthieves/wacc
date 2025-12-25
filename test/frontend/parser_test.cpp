6669b17bf0150c5b7c67a9b2f79ce4bd6ed33e14
saladthieves <saladthieves@gmail.com>
2025-12-25 12:28:08 +0300

    Added formatters to improve Parser expression testing

--- test/frontend/parser_test.cpp
+++ test/frontend/parser_test.cpp
@@ -1,32 +1,33 @@
 #include "ast.hpp"
 #include "base_test.hpp"
+#include "formatters.hpp"
+#include "matchers.hpp"
 #include "parser.hpp"
 #include "source.hpp"
-#include "test_utils.hpp"
 #include "token.hpp"
 
-#include <format>
 #include <gtest/gtest.h>
 #include <memory>
 #include <print>
 #include <string>
-#include <tuple>
 #include <vector>
 
 using enum wacc::front::token::TokenType;
 using enum wacc::front::ast::AstUnaryOpType;
+using enum wacc::front::ast::AstBinaryOpType;
+
 using wacc::front::parse::Parser;
 using wacc::front::src::Source;
 using wacc::front::token::Token;
-using wacc::front::token::TokenType;
-using wacc::test::utils::as;
+using wacc::test::fmt::formatAstExpr;
 
 using namespace wacc::front::ast;
+using namespace wacc::test::match;
 
 using std::make_unique;
+using std::pair;
 using std::string;
 using std::string_view;
-using std::tuple;
 using std::vector;
 
 using Tokens = vector<Token>;
@@ -52,49 +53,25 @@ TEST_F(ParserTest, parseThrowOnEmpty) {
     ASSERT_TRUE(error.contains("No tokens found"));
 }
 
-TEST_F(ParserTest, parseInvalidProgram) {
-    // ARRANGE
-    const auto tests = vector<tuple<string, TokenType, TokenType>>{
-        {"15",                          KEYWORD_INT,           CONSTANT_INT},
-        {"int ;",                       TokenType::IDENTIFIER, SEMICOLON   },
-        {"int main",                    OPEN_PAREN,            END         },
-        {"int main ()",                 KEYWORD_VOID,          CLOSE_PAREN },
-        {"int main (void)}",            OPEN_BRACE,            CLOSE_BRACE },
-        {"int main (void){}",           KEYWORD_RETURN,        CLOSE_BRACE },
-        {"int main (void){ 15; }",      KEYWORD_RETURN,        CONSTANT_INT},
-        {"int main (void){ return 42}", SEMICOLON,             CLOSE_BRACE },
-    };
-
-    for (const auto& test : tests) {
-        // ACT
-        auto parser = getParser(std::get<string>(test));
-        string error{};
-
-        try {
-            parser.parse();
-        } catch (const std::runtime_error& ex) {
-            error = ex.what();
-        }
-
-        // ASSERT
-        ASSERT_FALSE(error.empty());
-        ASSERT_TRUE(error.contains("ParserError"));
-
-        auto expected = std::get<1>(test);
-        ASSERT_TRUE(error.contains(std::format("Expected [{}]", expected)));
-
-        auto actual = std::get<2>(test);
-        ASSERT_TRUE(error.contains(std::format("but got [{}]", actual)));
-    }
-}
-
 TEST_F(ParserTest, parseMalformedExpression) {
     // ARRANGE
     const auto tests = vector<string>{
-        "int main(void) { return; }",      "int main(void) { return (); }",
-        "int main(void) { return (()); }", "int main(void) { return -; }",
-        "int main(void) { return ~; }",    "int main(void) { return -(); }",
-        "int main(void) { return ~(); }",  "int main(void) { return --15; }",
+        // clang-format off
+        "int main(void) { return; }",      
+        "int main(void) { return (); }",
+        "int main(void) { return (()); }", 
+        "int main(void) { return -; }",
+        "int main(void) { return ~; }",    
+        "int main(void) { return -(); }",
+        "int main(void) { return ~(); }",  
+        "int main(void) { return --15; }",
+        "int main(void) { return 1 + ; }",
+        "int main(void) { return 2 - /; }",
+        "int main(void) { return 8 % --; }",
+        "int main(void) { return -1 / --; }",
+        "int main(void) { return (2 *); }",
+        "int main(void) { return ((8 %); }",
+        // clang-format on
     };
 
     for (const auto& test : tests) {
@@ -111,8 +88,7 @@ TEST_F(ParserTest, parseMalformedExpression) {
         // ASSERT
         ASSERT_FALSE(error.empty());
         ASSERT_TRUE(error.contains("ParserError"));
-        ASSERT_TRUE(error.contains("malformed expression") ||
-                    error.contains("Expected") ||
+        ASSERT_TRUE(error.contains("malformed") || error.contains("Expected") ||
                     error.contains("Cannot parse"));
     }
 }
@@ -121,61 +97,188 @@ TEST_F(ParserTest, parseProgram) {
     // ARRANGE
     const auto source =
         R"(int main(void) {
-        return ~(-42);
-    }
-    )";
+                return 3 + 9 * 8 - 7 / 1 % 3;
+           }
+        )";
+
     auto parser = getParser(source);
 
     // ACT
     auto ptr = parser.parse();
 
     // ASSERT
-    auto program = as<AstProg>(ptr);
-
-    auto function = as<AstFun>(program->function);
-    auto functionName = as<AstIdent>(function->name);
-    ASSERT_TRUE(functionName->value == "main");
-
-    auto functionBody = as<AstReturn>(function->body);
-    auto returnExprUnary = as<AstUnary>(functionBody->expr);
-    ASSERT_EQ(returnExprUnary->op, UNARY_COMPLEMENT);
-    auto returnExprNeg = as<AstUnary>(returnExprUnary->expr);
-    ASSERT_EQ(returnExprNeg->op, UNARY_NEGATE);
-    auto returnExprNegVal = as<AstConstInt>(returnExprNeg->expr);
-    ASSERT_TRUE(returnExprNegVal->value == 42);
-
-    std::println("{}", ptr);
+    const auto& body = matchAstProg(ptr);
+    matchAstReturn(body, [](auto& expr) {
+        matchAstBinary(expr, [](auto& op, auto& left, auto& right) {
+            ASSERT_EQ(op, AstBinaryOpType::BINARY_ADD);
+            matchAstConstInt(left, 3);
+            matchAstBinary(right, [](auto& op, auto& left, auto& right) {
+                ASSERT_EQ(op, AstBinaryOpType::BINARY_REMAINDER);
+                matchAstConstInt(right, 3);
+                matchAstBinary(left, [](auto& op, auto& left, auto& right) {
+                    ASSERT_EQ(op, AstBinaryOpType::BINARY_DIVIDE);
+                    matchAstConstInt(right, 1);
+                    matchAstBinary(left, [](auto& op, auto& left, auto& right) {
+                        ASSERT_EQ(op, AstBinaryOpType::BINARY_SUBTRACT);
+                        matchAstConstInt(right, 7);
+                        matchAstBinary(left, [](auto& op, auto& l, auto& r) {
+                            ASSERT_EQ(op, AstBinaryOpType::BINARY_MULTIPLY);
+                            matchAstConstInt(l, 9);
+                            matchAstConstInt(r, 8);
+                        });
+                    });
+                });
+            });
+        });
+    });
 }
 
-TEST_F(ParserTest, parsePrograms) {
+TEST_F(ParserTest, parseConstant) {
     // ARRANGE
-    const auto tests = vector<string>{
-        "int main(void) { return -15; }",
-        "int main(void) { return ~30; }",
-        "int main(void) { return ~~45; }",
-        "int main(void) { return ~-15; }",
-        "int main(void) { return -~80; }",
-        "int main(void) { return -~~100; }",
-        "int main(void) { return -(~~200); }",
-        "int main(void) { return ~-(~300); }",
-        "int main(void) { return (~15); }",
-        "int main(void) { return ~(15); }",
-        "int main(void) { return ~~(-3); }",
-        "int main(void) { return ~(-~(~-(~~-15))); }",
+    const auto tests = vector<pair<string, string>>{
+        {"int main(void) { return 0; }",                      "0" },
+        {"int main(void) { return 15; }",                     "15"},
+        {"int main(void) { return 23; }",                     "23"},
+        {"int main(void) { return (15); }",                   "15"},
+        {"int main(void) { return ((33)); }",                 "33"},
+        {"int main(void) { return (((8))); }",                "8" },
+        {"int main(void) { return ((((((((((15)))))))))); }", "15"},
     };
 
-    for (const auto& test : tests) {
-        auto parser = getParser(test);
-        string error{};
+    for (const auto& pair : tests) {
+        auto parser = getParser(pair.first);
+        const auto& content = pair.second;
 
         // ACT
-        try {
-            parser.parse();
-        } catch (const std::runtime_error& ex) {
-            error = ex.what();
-        }
+        auto ptr = parser.parse();
 
         // ASSERT
-        ASSERT_TRUE(error.empty());
+        const auto& body = matchAstProg(ptr);
+        matchAstReturn(body, [&](auto& expr) {
+            string data = formatAstExpr(expr);
+            ASSERT_STREQ(data.c_str(), content.c_str());
+        });
+    }
+}
+
+TEST_F(ParserTest, parseUnary) {
+    // ARRANGE
+    const auto tests = vector<pair<string, string>>{
+        {"int main(void) { return -1; }",          "[-1]"               },
+        {"int main(void) { return -23; }",         "[-23]"              },
+        {"int main(void) { return -42; }",         "[-42]"              },
+        {"int main(void) { return ~6; }",          "[~6]"               },
+        {"int main(void) { return ~88; }",         "[~88]"              },
+        {"int main(void) { return ~90; }",         "[~90]"              },
+        {"int main(void) { return -~1; }",         "[-[~1]]"            },
+        {"int main(void) { return ~-22; }",        "[~[-22]]"           },
+        {"int main(void) { return ~~40; }",        "[~[~40]]"           },
+        {"int main(void) { return -(~55); }",      "[-[~55]]"           },
+        {"int main(void) { return ~((-31)); }",    "[~[-31]]"           },
+        {"int main(void) { return ~~(-(~~~9)); }", "[~[~[-[~[~[~9]]]]]]"},
+    };
+
+    for (const auto& pair : tests) {
+        auto parser = getParser(pair.first);
+        const auto& content = pair.second;
+
+        // ACT
+        auto ptr = parser.parse();
+
+        // ASSERT
+        const auto& body = matchAstProg(ptr);
+        matchAstReturn(body, [&](auto& expr) {
+            string data = formatAstExpr(expr);
+            ASSERT_STREQ(data.c_str(), content.c_str());
+        });
+    }
+}
+
+TEST_F(ParserTest, parseBinaryBasic) {
+    // ARRANGE
+    const auto tests = vector<pair<string, string>>{
+        {"int main(void) { return 1 + 1; }",       "[1 + 1]"       },
+        {"int main(void) { return 3 - 2; }",       "[3 - 2]"       },
+        {"int main(void) { return 11 * 5; }",      "[11 * 5]"      },
+        {"int main(void) { return 8 / 2; }",       "[8 / 2]"       },
+        {"int main(void) { return 11 % 3; }",      "[11 % 3]"      },
+        {"int main(void) { return -3 + 5; }",      "[[-3] + 5]"    },
+        {"int main(void) { return 8 - ~9; }",      "[8 - [~9]]"    },
+        {"int main(void) { return -72 - -9; }",    "[[-72] - [-9]]"},
+        {"int main(void) { return (~70) * (3); }", "[[~70] * 3]"   },
+    };
+
+    for (const auto& pair : tests) {
+        auto parser = getParser(pair.first);
+        const auto& content = pair.second;
+
+        // ACT
+        auto ptr = parser.parse();
+
+        // ASSERT
+        const auto& body = matchAstProg(ptr);
+        matchAstReturn(body, [&](auto& expr) {
+            string data = formatAstExpr(expr);
+            ASSERT_STREQ(data.c_str(), content.c_str());
+        });
+    }
+}
+
+TEST_F(ParserTest, parseBinaryAssoc) {
+    // ARRANGE
+    const auto tests = vector<pair<string, string>>{
+        {"int main(void) { return 1 + 2 + 3; }",         "[[1 + 2] + 3]"      },
+        {"int main(void) { return 4 + 5 - 6 + 7; }",     "[[[4 + 5] - 6] + 7]"},
+        {"int main(void) { return 1 + (3 - 5); }",       "[1 + [3 - 5]]"      },
+        {"int main(void) { return 0 + (2 + 4) - 9; }",   "[[0 + [2 + 4]] - 9]"},
+        {"int main(void) { return (3 - (5 + 0)) - 7; }", "[[3 - [5 + 0]] - 7]"},
+        {"int main(void) { return 2 + ((3) + 9); }",     "[2 + [3 + 9]]"      },
+        {"int main(void) { return 1 * 3; }",             "[1 * 3]"            },
+        {"int main(void) { return 2 / -9; }",            "[2 / [-9]]"         },
+        {"int main(void) { return ~13 % (-~7); }",       "[[~13] % [-[~7]]]"  },
+    };
+
+    for (const auto& pair : tests) {
+        auto parser = getParser(pair.first);
+        const auto& content = pair.second;
+
+        // ACT
+        auto ptr = parser.parse();
+
+        // ASSERT
+        const auto& body = matchAstProg(ptr);
+        matchAstReturn(body, [&](auto& expr) {
+            string data = formatAstExpr(expr);
+            ASSERT_STREQ(data.c_str(), content.c_str());
+        });
+    }
+}
+
+TEST_F(ParserTest, parseBinPrecedence) {
+    // ARRANGE
+    const auto tests = vector<pair<string, string>>{
+        // clang-format off
+        {"int main(void) { return 1 + 2 * 3; }", "[1 + [2 * 3]]" },
+        {"int main(void) { return 1 * 2 - 3; }", "[[1 * 2] - 3]"},
+        {"int main(void) { return ~5 * 4 - -8; }", "[[[~5] * 4] - [-8]]"},
+        {"int main(void) { return (-16) % (~4 + ~~8); }", "[[-16] % [[~4] + [~[~8]]]]"},
+        {"int main(void) { return 1 * 2 - 3 * (4 + 5); }", "[[[1 * 2] - 3] * [4 + 5]]"},
+        {"int main(void){return 3 + 9 * 8 - 7 / 1 % 3; }", "[3 + [[[[9 * 8] - 7] / 1] % 3]]"},
+        // clang-format on
+    };
+
+    for (const auto& pair : tests) {
+        auto parser = getParser(pair.first);
+        const auto& content = pair.second;
+
+        // ACT
+        auto ptr = parser.parse();
+
+        // ASSERT
+        const auto& body = matchAstProg(ptr);
+        matchAstReturn(body, [&](auto& expr) {
+            string data = formatAstExpr(expr);
+            ASSERT_STREQ(data.c_str(), content.c_str());
+        });
     }
 }
