d84805cb9d32fc3c3c4fd3124ecee4522fc4042b
saladthieves <saladthieves@gmail.com>
2025-12-27 13:14:47 +0300

    Add testing formatters for Asm

--- test/base/formatters.hpp
+++ test/base/formatters.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "asm_ast.hpp"
 #include "ast.hpp"
 #include "tacky_ast.hpp"
 
@@ -11,11 +12,15 @@ namespace fmt {
 namespace {
 using namespace wacc::front::ast;
 using namespace wacc::tacky::ast;
+using namespace wacc::back::ast;
 } // namespace
+// TODO: Split into three different files
 
 std::string formatAstExpr(const AstExprPtr& ptr);
 
 std::string formatTackyInstr(const TackyInstrPtr& ptr);
+
+std::string formatAsmInstr(const AsmInstrPtr& ptr);
 
 namespace {
 // Ast
@@ -31,6 +36,18 @@ std::string formatTackyBinary(const TackyBinary& ast);
 std::string formatTackyVal(const TackyVal& ast);
 std::string formatTackyUnaryOp(const TackyUnaryOpType& type);
 std::string formatTackyBinaryOp(const TackyBinaryOpType& type);
+// Asm
+std::string formatAsmOperand(const AsmOperand& op);
+std::string formatAsmRegister(const AsmRegisterType& type);
+std::string formatAsmMov(const AsmMov& mov);
+std::string formatAsmUnary(const AsmUnary& unary);
+std::string formatAsmBinary(const AsmBinary& binary);
+std::string formatAsmIdiv(const AsmIdiv& idiv);
+std::string formatAsmCdq();
+std::string formatAsmAllocStack(const AsmAllocStack& stack);
+std::string formatAsmRet();
+std::string formatAsmUnaryOp(const AsmUnaryOpType& type);
+std::string formatAsmBinaryOp(const AsmBinaryOpType& type);
 } // namespace
 
 } // namespace fmt
