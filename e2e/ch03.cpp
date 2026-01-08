b031c0d8aaf8a41dbd49c224f9dab41011e0bca3
saladthieves <saladthieves@gmail.com>
2026-01-08 12:44:31 +0300

    Complete chapter 3 (extra credit)

--- e2e/ch03.cpp
+++ e2e/ch03.cpp
@@ -11,37 +11,74 @@ protected:
     static constexpr auto chapter = "ch03";
 };
 
-TEST_F(Chapter3Test, chapter3e2e) {
+TEST_F(Chapter3Test, chapter3e2eArith) {
+    // ARRANGE
     // ARRANGE
     vector<pair<string, int>> tests{
-        // clang-format off
-        {"{ return 0 + 0; }", 0},
-        {"{ return 1 + 2; }", 3},
-        {"{ return 3 - 1; }", 2},
-        {"{ return 5 - 2; }", 3},
-        {"{ return 2 * 3; }", 6},
-        {"{ return 12 * 3; }", 36},
-        {"{ return 5 / 2; }", 2},
-        {"{ return 8 / 4; }", 2},
-        {"{ return 5 % 2; }", 1},
-        {"{ return 9 % 3; }", 0},
-        {"{ return 1 + 2 + 3; }", 6},
-        {"{ return 9 - 4 - 1; }", 4},
-        {"{ return 12 + 1 - 3; }", 10},
-        {"{ return 11 - 1 + 8; }", 18},
-        {"{ return 3 * 3 * 3; }", 27},
-        {"{ return 64 / 8 / 8; }", 1},
-        {"{ return 9 * 3 / 3; }", 9},
-        {"{ return 64 / 8 * 2; }", 16},
-        {"{ return 8 % 4 * 10; }", 0},
-        {"{ return 1 + 2 * 3; }", 7},
-        {"{ return 1 / 2 + 3; }", 3},
-        {"{ return 2 * 4 - 7; }", 1},
-        {"{ return 40 - 5 % 7; }", 35},
-        {"{ return (2 + 8) / 4; }", 2},
-        {"{ return (2 + 8) / 4 + 5; }", 7},
-        {"{ return -2 + 5 * (-(-32) / 8); }", 18},
-        // clang-format on
+        {"{ return 0 + 0; }",                         0 },
+        {"{ return 1 + 2; }",                         3 },
+        {"{ return 3 - 1; }",                         2 },
+        {"{ return 5 - 2; }",                         3 },
+        {"{ return 2 * 3; }",                         6 },
+        {"{ return 12 * 3; }",                        36},
+        {"{ return 5 / 2; }",                         2 },
+        {"{ return 8 / 4; }",                         2 },
+        {"{ return 5 % 2; }",                         1 },
+        {"{ return 9 % 3; }",                         0 },
+        {"{ return 1 + 2 + 3; }",                     6 },
+        {"{ return 9 - 4 - 1; }",                     4 },
+        {"{ return 12 + 1 - 3; }",                    10},
+        {"{ return 11 - 1 + 8; }",                    18},
+        {"{ return 3 * 3 * 3; }",                     27},
+        {"{ return 64 / 8 / 8; }",                    1 },
+        {"{ return 9 * 3 / 3; }",                     9 },
+        {"{ return 64 / 8 * 2; }",                    16},
+        {"{ return 8 % 4 * 10; }",                    0 },
+        {"{ return 1 + 2 * 3; }",                     7 },
+        {"{ return 1 / 2 + 3; }",                     3 },
+        {"{ return 2 * 4 - 7; }",                     1 },
+        {"{ return 40 - 5 % 7; }",                    35},
+        {"{ return (2 + 8) / 4; }",                   2 },
+        {"{ return (2 + 8) / 4 + 5; }",               7 },
+        {"{ return -2 + 5 * (-(-32) / 8); }",         18},
+    };
+
+    for (const auto& pair : tests) {
+        // ACT
+        const auto expected = std::get<int>(pair);
+
+        auto code = std::format("int main(void) {}", std::get<string>(pair));
+        auto compiled = compile(code, chapter);
+        EXPECT_TRUE(compiled.has_value());
+
+        // ASSERT
+        auto binaryPath = compiled.value();
+        auto actual = run(binaryPath);
+        ASSERT_EQ(actual, expected);
+    }
+}
+
+TEST_F(Chapter3Test, chapter3e2eBit) {
+    // ARRANGE
+    vector<pair<string, int>> tests{
+        {"{ return 3 << 1; }",                        6 },
+        {"{ return 3 * 1 << 2; }",                    12},
+        {"{ return 2 * 1 << 2 + 1; }",                16},
+        {"{ return 2 >> 1; }",                        1 },
+        {"{ return 16 / 2 >> 2; }",                   2 },
+        {"{ return 14 >> 2 + 1; }",                   1 },
+        {"{ return 18 - 2 >> 2 - 1; }",               8 },
+        {"{ return 2 << 2 >> 2; }",                   2 },
+        {"{ return 28 - 8 >> 2 * 1 + 1; }",           2 },
+        {"{ return 2 & 3; }",                         2 },
+        {"{ return 3 ^ 2; }",                         1 },
+        {"{ return 3 | 2; }",                         3 },
+        {"{ return 2 * 3 & 3 / 4; }",                 0 },
+        {"{ return 5 % 2 ^ 3; }",                     2 },
+        {"{ return 8 - 4 | 4 + 8; }",                 12},
+        {"{ return 4 & 3 | 2 ^ 1; }",                 3 },
+        {"{ return 2 + 3 & 8 / 9 | 7 - 5 ^ 5 % 5; }", 2 },
+        {"{ return 3 << 1 & 8 >> 2 | 1; }",           3 },
     };
 
     for (const auto& pair : tests) {
--- sample/file.c
+++ sample/file.c
@@ -1,3 +1,3 @@
 int main(void) {
-    return 32 / 2 >> 1 * 2;
+    return 5 / 2;
 }
